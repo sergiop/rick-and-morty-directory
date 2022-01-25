7f9bfa4a7275a357a72bd6c59f3148e094976886
Sergio Pedercini <sergio.pedercini@gmail.com>
2022-01-25 05:45:12 +0100

    Add infitite scroll on characters page.

--- src/routes/characters/CharactersRoot.tsx
+++ src/routes/characters/CharactersRoot.tsx
@@ -1,27 +1,65 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import useInfiniteScroll from 'react-infinite-scroll-hook'
 
 import { Loading } from '../../components/Loading/Loading'
 import type { Character, PaginatedResource } from '../../types'
 import { get } from '../../utils/api'
+import { getPageNumber } from '../../utils/utils'
 import { CharacterItem } from './CharacterItem'
 
-export const CharactersRoot = () => {
-  const [characters, setCharacters] = useState<Character[]>()
+const hasNextPage = (results: PaginatedResource<Character[]>): boolean => {
+  if (results.info && results.info.next) {
+    return true
+  }
 
-  const getCharacters = () => {
-    get<PaginatedResource<Character[]>>('/character')
+  return false
+}
+
+export const CharactersRoot = () => {
+  const [characters, setCharacters] = useState<PaginatedResource<Character[]>>()
+  const [charactersIsLoading, setCharactersIsLoading] = useState(false)
+
+  const getCharacters = (pageNumber: number) => {
+    setCharactersIsLoading(true)
+    get<PaginatedResource<Character[]>>(`/character?page=${pageNumber}`)
       .then((data) => {
-        setCharacters(data.results)
+        setCharacters({
+          ...characters,
+          info: data.info,
+          results: [
+            ...(characters && characters.results ? characters.results : []),
+            ...(data && data.results ? data.results : []),
+          ],
+        })
+        setCharactersIsLoading(false)
       })
   }
 
-  useEffect(() => {
-    getCharacters()
-  }, [])
+  const nextPageNumber = (characters
+    && characters.info
+    && characters.info.next)
+    ? getPageNumber(characters.info.next) : 1
 
-  return characters ? (
+  const [sentryRef] = useInfiniteScroll({
+    loading: charactersIsLoading,
+    hasNextPage: characters ? hasNextPage(characters) : true,
+    onLoadMore: () => getCharacters(nextPageNumber),
+    rootMargin: '0px 0px 400px 0px',
+  })
+
+  return (
     <>
-      {characters.map((c) => <CharacterItem character={c} key={c.id} />)}
+      {
+        characters
+        && characters.results
+        && characters.results.map((c) => <CharacterItem character={c} key={c.id} />)
+      }
+
+      {(charactersIsLoading || hasNextPage) && (
+        <div ref={sentryRef}>
+          <Loading />
+        </div>
+      )}
     </>
-  ) : <Loading />
+  )
 }
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,8 @@
 export const getCharacterEpisodesIds = (episodes: string[]): string[] => episodes.map((e) => e.split('/').slice(-1)).flat()
 
 export const getLocationId = (location: string): string => location.split('/').slice(-1).toString()
+
+export const getPageNumber = (url: string): number => {
+  const u = new URL(url)
+  return Number(u.search.split('=').slice(-1))
+}
